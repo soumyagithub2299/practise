1481eceead75e6d7953b50eb020306a38395ac79
Soumya Tiwari <soumyatiwari7247@gmail.com>
2023-09-20 12:02:03 +0530

    done react quill

--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Help.css'; // Import the CSS file
-
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; 
 
 const Help = () => {
+  const [editorHtml, setEditorHtml] = useState('');
+
+  const modules = {
+    toolbar: [
+      [{ 'header': '1' }, { 'header': '2' }],
+      ['bold', 'italic', 'underline', 'strike'],
+      ['link', 'image', 'video'],
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      ['clean']
+    ],
+  };
+
+  const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike',
+    'list', 'bullet',
+    'link', 'image', 'video'
+  ];
+
+  const handleEditorChange = (html) => {
+    setEditorHtml(html);
+  };
+
   return (
     <div className="help-container">
-      <Paper elevation={3} className="help-paper">
+      <Paper elevation={9} className="help-paper">
         <Typography variant="h4" className="help-title">
           Help Center
         </Typography>
-        <Typography variant="body1" className="help-content">
-          If you need assistance or have any questions, please feel free to contact our
-          support team at support@example.com.
-        </Typography>
+        <ReactQuill
+          theme="snow" // You can choose different themes if needed
+          modules={modules}
+          formats={formats}
+          value={editorHtml}
+          onChange={handleEditorChange}
+          className="help-editor"
+        />
       </Paper>
     </div>
   );
